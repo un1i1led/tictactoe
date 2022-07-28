4d8ef04302bd24313742e3c71a6843f0def97f76
gilberto <gsqwow@gmail.com>
2022-07-27 20:02:27 -0600

    Add display functionality

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-const gameboard = (() => {
-    const targetDiv = document.querySelector('.board');
-
+const Gameboard = (() => {
     let board = ['', '', '', '', '', '', '', '', ''];
 
-    const display = (target) => {
-        for (i = 0; i < 9; i++){
-            const arrayDiv = document.createElement('div');
-            arrayDiv.className = 'array-div';
-            arrayDiv.innerHTML = target[i];
-            arrayDiv.setAttribute('data-id', i);
-            targetDiv.appendChild(arrayDiv);
-        }
-    }
+    const boardDiv = document.querySelector('.board');
+    const remove = () => {
+        boardDiv.remove();
+    };
 
-    display(board);
+    const display = () => {
+        for (let i = 0; i < board.length; i++){
+            const cell = document.createElement('div');
+            cell.setAttribute('data-id', i);
+            cell.className = 'array-div';
+            cell.innerHTML = board[i];
+            boardDiv.appendChild(cell);
+        };
+    };
 
+    display();
     return {
-        display
+        board,
+        display,
+        remove
     }
+})();
+
+const Player = (name, marker) => {
+    return {
+        name,
+        marker
+    }
+};
+
+const Game = (() => {
+    const player1 = Player('X', 'X');
+    const player2 = Player('O', 'O');
+    let currentPlayer = player1;
+
+    const mark = (item) => {
+        Gameboard.remove();
+        Gameboard.board[0] = currentPlayer.marker;
+        Gameboard.display();
+    };
+
+    document.querySelectorAll('.array-div').forEach(item => {
+        item.addEventListener('click', function(){
+            mark(item);
+        });
+    });
+
 
 })();
